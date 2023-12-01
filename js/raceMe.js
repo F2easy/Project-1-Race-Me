f5dea128b4f028e8ef7c75d6fb30509b6583f248
Felix-F <felixferreras2002@gmail.com>
2023-12-01 12:53:18 -0500

    got some DOM manipulation down pact

--- js/raceMe.js
+++ js/raceMe.js
@@ -3,20 +3,20 @@
 
 
 ///functions
-function diceRoll(){  /// roles the dice so players can progress
+function diceRoll(){  /// roles the dice so players can progress //attach to roll the dice button
     let p = Math.floor(Math.random() * 6) + 1
     console.log(p)
     return p
 };
 
-function coinFlip(){   ///this Function flips a coin that ultimately starts the game
+function coinFlip(){   ///this Function flips a coin that ultimately starts the game //attach to run it button
 const rand = Math.random
 if (rand < .5){
     return "Heads";
 } else { return "Tails"
 }}
 
-function start(){
+function start(){  
     let flip = diceRoll()
     while (!flip == 5 || !flip == 3){   // ask if "if statement is good" or while loop would be better
         flip = diceRoll() 
@@ -32,7 +32,8 @@ const p1 = document.getElementById('.player1') ; ///Ask if I am selecting these 
 const p2 = document.getElementById('.player2') ; 
 
 //board and special board spots
-const gameBoardArr = [0,0,0,0,0,0,0,0,0,0,0] /// should I make my gameBoard an array or ? and then create a turn function in the game
+///                 sp0 sp1 sp2 sp3 sp4 sp5 sp6 sp7 sp8 sp9 sp10 sp11
+const gameBoardArr = [0, 0, 0, 0, 0,  0,  0,  0,  0,  0,  0,  0,  0  ] ///  sp0 -> start   sp6 -> luckySpot  sp7 --> Trap Spot sp11
 
 const gameBoard = document.getElementsByClassName("sp"); //grabbing the entire board
 const trapSpot = document.getElementById('trap'); // grabbing the trap spot
@@ -55,6 +56,7 @@ const luckySpot = document.getElementById('lucky'); // grabbing the lucky spot
 
 // to make players move write a function that matches player position to player ID 
 //code the start button
+/// Event Listeners ///
 const startGame = document.getElementById("run-it");
 //startGame.addEventListener('click', start()) ///?
     
@@ -69,11 +71,42 @@ const startGame = document.getElementById("run-it");
 // code the startOver logic that goes with trapSpot and capture
 //code the start over button
 // code the diceRoll Button 
+function init() {
+
+let turn = 0
+let winner = null
+
+    ///            sp0 sp1 sp2 sp3 sp4 sp5 sp6 sp7 sp8 sp9 sp10 sp11
+const gameBoardArr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] ///  sp0 -> start   sp6 -> luckySpot  sp7 --> Trap Spot sp11 --> winning spot
+
+// Call the render Function once the the RENDER is created
+}
+
+function renderBoard(){
+// loop over array that reperesents the game board                            
+// change the background of the slot to the player image to simulate movement
+    gameBoardArr.forEach((colArr, sptIdx) => {
+  //      console.log("sp"+sptIdx) //this is the index or place that is in
+ //       console.log(colArr) // this is the value inside the array
+
+        //determine the Id of the element
+      //  const cellId = `sp${sptIdx}${" "}${colArr}`  //now we use this ID to select specific DIV's and make something happen to them and store values
+        const cellId = `sp${sptIdx}`
+        console.log(cellId)                                           //then use render function to make specific changes on the DOM
+        
+        const CellEl = document.getElementById(cellId)
+        console.log('CellEL', CellEl)
+})
+}
+
+renderBoard()
 
 
 
 
 
-///constants
 
 
+function render(){
+
+}
