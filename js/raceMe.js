c6dc379b35ef79b2267bce3ef2bd0857c70e243f
Felix-F <felixferreras2002@gmail.com>
2023-12-01 09:08:59 -0500

    morning commit for changes from last night

--- js/raceMe.js
+++ js/raceMe.js
@@ -17,24 +17,22 @@ if (rand < .5){
 }}
 
 function start(){
-    const flip = 0;
-    diceRoll(flip);
-    if (flip == 5 || flip == 3){   // ask if "if statement is good" or while loop would be better
-        return "Start" 
+    let flip = diceRoll()
+    while (!flip == 5 || !flip == 3){   // ask if "if statement is good" or while loop would be better
+        flip = diceRoll() 
     }
-    else return "Try Again"
 }
 
 /*-----Identify and initilize our state Variable---*/
 //starting position
 let postion1 = 0;
-let position2 = 0;
+let position2 = 0; /// the number in the position can correlate to the Element Id so i can use the position to target ID and use that number for movement
 // Players
 const p1 = document.getElementById('.player1') ; ///Ask if I am selecting these 2 correctly 
 const p2 = document.getElementById('.player2') ; 
 
 //board and special board spots
-// const gameBoardArr = [0,0,0,0,0,0,0,0,0,0,0] /// should I make my gameBoard an array or ? and then create a turn function in the game
+const gameBoardArr = [0,0,0,0,0,0,0,0,0,0,0] /// should I make my gameBoard an array or ? and then create a turn function in the game
 
 const gameBoard = document.getElementsByClassName("sp"); //grabbing the entire board
 const trapSpot = document.getElementById('trap'); // grabbing the trap spot
@@ -42,15 +40,23 @@ const luckySpot = document.getElementById('lucky'); // grabbing the lucky spot
 
 
 ///Event Listeners
-
+//Write a function that matches player position to an element id
+// for example
+// if player position = 0
+// sp1 would have the image in it
+// if player position = 3
+// sp4 would have the image in it (edited) 
+// function movePlayer(){
+//     if()
+// }
 
 //these are our constants  and are pertaining to yhe state of our game (staring values)
 // These are the starting point of our game and can be initialized and changed throughout
 
-
+// to make players move write a function that matches player position to player ID 
 //code the start button
 const startGame = document.getElementById("run-it");
-startGame.addEventListener('click', start()) ///?
+//startGame.addEventListener('click', start()) ///?
     
 
 //create a function that makes you roll a 5 or 3 in order to start 
