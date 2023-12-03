45fc9859b778e4164e7e781e43f4cf603dc6d9bf
Felix-F <felixferreras2002@gmail.com>
2023-12-02 22:39:55 -0500

    major changes to winning logic and turn logic that impact HTML

--- js/raceMe.js
+++ js/raceMe.js
@@ -2,41 +2,29 @@
 
 
 
-///functions
 
 
-function coinFlip(){   ///this Function flips a coin that ultimately starts the game //attach to run it button
-const rand = Math.random
-if (rand < .5){
-    return "Heads";
-} else { return "Tails"
-}}
-
-function start(){  
-    let flip = diceRoll()
-    while (!flip == 5 || !flip == 3){   // ask if "if statement is good" or while loop would be better
-        flip = diceRoll() 
-    }
-}
 
 /*-----Identify and initilize our state Variable---*/
 
 
 ///state variables
 
-let turn = 0  // will always be 1 for p1 or 2 for p2 
-let winner = null // will either be p1 or p2
+let turn = 1 || 2 || null// will always be 1 for p1 or 2 for p2 
+let winner = 1 || 2 || null// will either be p1 or p2 if neither
 const board = ['sp0','sp1','sp2','sp3','sp4','sp5','sp6','sp7','sp8', 'sp9','sp10','sp11' ];
+
 ///
 // cached DOM elements
 /// grab our HTML elements and save them to variables to use later
-const messageEl = document.querySelectorAll('h2') 
+const messageEl = document.querySelector('h2')
+const turnEl = document.querySelector('h3')
 const gameBoard = document.getElementsByClassName("sp"); //grabbing the entire board
 const trapSpot = document.getElementById('trap'); // grabbing the trap spot
 const luckySpot = document.getElementById('lucky'); // grabbing the lucky spot
-const runItBackButton = document.getElementById('runItBack'); //grabbing run it back / restart button
+const restartButton = document.getElementById('runItBack'); //grabbing run it back / restart button
 const startButton = document.getElementById('start') // grabbing start button
-const  rollDiceButton = document.getElementById('rollDice') // grabbing roll the dice button
+const rollDiceButton = document.getElementById('rollDice') // grabbing roll the dice button
 
 
 ///Event Listeners
@@ -56,7 +44,7 @@ const  rollDiceButton = document.getElementById('rollDice') // grabbing roll the
 // to make players move write a function that matches player position to player ID 
 //code the start button
 /// Event Listeners ///
-const startGame = document.getElementById("run-it");
+
 //startGame.addEventListener('click', start()) ///?
     
 
@@ -85,61 +73,113 @@ console.log("this is p1 or player1",p1)
 
 
 
-function diceRoll(){  /// roles the dice so players can progress //attach to roll the dice button
-    return Math.floor(Math.random() * 6) + 1 ;
+function init(){
+
+turn = 0 
+winner = 0 
+
+const board = ['sp0','sp1','sp2','sp3','sp4','sp5','sp6','sp7','sp8', 'sp9','sp10','sp11' ]
+
+render()
 }
-let currentPostion = 0; // position of p1
-//let position2 = 0;  // position of p2
-function movePlayer(player){
-    let rollDice = diceRoll();
-    currentPostion += rollDice;
-    if(currentPostion >= board.length){
-        currentPostion -= board.length;
-    }
-    let current = gameBoard[currentPostion]    ///this function rolls the dice and moves players that is selected
-current.append(player)
-} //However right now it only moves p1 as listed I want to make this function able to move whatever piece it is attached to
+render()
 
-movePlayer(p) /// if you put p1 or p2 into this function it will move the players to a random block based on the dice role
-
-
-// init initialize an empty game and init runs one time whe the dice rolls
-// then the init function will be called again from the the play again / runit back button
-function init() {
-}
 
 // renders the game board
 function renderBoard(){
 // loop over array that reperesents the game board                            
 // change the background of the slot to the player image to simulate movement
-    gameBoardArr.forEach((colArr, sptIdx) => {
-  //      console.log("sp"+sptIdx) //this is the index or place that is in
- //       console.log(colArr) // this is the value inside the array
 
-        //determine the Id of the element
-      //  const cellId = `sp${sptIdx}${" "}//${colArr}`  //now we use this ID to select specific DIV's and make something happen to them and store values
-        // const cellId = `sp${sptIdx}`
-        // console.log(cellId)                                           //then use render function to make specific changes on the DOM
-        
-        // const CellEl = document.getElementById(cellId)
-        // console.log('CellEL', CellEl)
-
-        // CellEl.style.backgroundColor=[]
-})
 }
+///functions
 
-// p1.forEach((player1)=> {
-//     player1.addEventListener()
-// })
+
+function coinFlip(){   ///this Function flips a coin that ultimately starts the game //attach to run it button
+    const rand = Math.random
+    if (rand < .5){
+        return "Heads";
+    } else { return "Tails"
+    }}
+    
+    function start(){  
+        let flip = diceRoll()
+        while (!flip == 5 || !flip == 3){   // ask if "if statement is good" or while loop would be better
+            flip = diceRoll() 
+        }
+    }
+    
+    function diceRoll(){  /// roles the dice so players can progress //attach to roll the dice button
+        return Math.floor(Math.random() * 6) + 1 ;
+    }
+    let currentPostion = 0; // position of p1
+    //let position2 = 0;  // position of p2
+    function movePlayer(player){
+        let rollDice = diceRoll();
+        currentPostion += rollDice;
+        if(currentPostion >= board.length){
+            currentPostion -= board.length;
+        }
+        let current = gameBoard[currentPostion]    ///this function rolls the dice and moves players that is selected
+    current.append(player)
+    } //However right now it only moves p1 as listed I want to make this function able to move whatever piece it is attached to
+    
+    movePlayer() /// if you put p1 or p2 into this function it will move the players to a random block based on the dice role
+// render()
+// init()
+
+
+
+
+
+
 // chnge intitial vis of the runItBack button
+
+// render board
+function renderBoard(){
+        // loop over the board to determine where the 
+
+    }
+
+    board.forEach((colArr, sptIdx) => {
+        const cellId = `sp${sptIdx}` // referring to spots via html name sp0, sp1, sp2 AKA cell ID
+      // console.log(cellId)                                           //then use render function to make specific changes on the DOM
+        const CellEl = document.getElementById(cellId)
+    // console.log('CellEL', CellEl)
+    })
+    
+
 
 // render controls --> changes the visibility of the run it back function
 
 // render message  --> will display whose turn it is
 
 // render --> calls all of our render based functions at once
-
+function render(){
+ // call renderBoard
+ // call renderMessage
+ renderMessage()
+ renderBoard()
+  renderControls()
+}
 // getWinner --> checks to see if we have a winner / if player lands on final spot
+function renderMessage() {
+    if (winner == 1){
+        messageEl.innerText = " Tiger Wins the Race !!"
+    } else if (winner == 2) {
+        messageEl.innerText = " Bear Wins the Race !!"
+    }
+    else  (winner == null) 
+    messageEl.innerText = " Who is going to win ? !!" 
+
+    if (turn == 1){
+        turnEl.innerText = "Tiger's Turn"
+    }
+    else if (turn == 2){
+        turnEl.innerText = "Bear's Turn"
+    }
+    else (turn == null)
+        turnEl.innerText = "Start Game !!"
+}
 
 
 //EventListeners
@@ -150,9 +190,10 @@ function renderBoard(){
 // restart / run it back button
 // player landing on same spot capture or re-roll option
 function renderControls(){
+ restartButton.style.visibility = winner ? 'visible' : 'hidden'  // why isn't this working ??
 
 
-    
+
 }
 
 
@@ -161,9 +202,7 @@ function renderControls(){
 
 
 
-function render(){
 
-}
 
 //              Game Logic      ///
 // -StarGame button starts the game
